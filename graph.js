66349b1e132cd3be5ebeace8c203ef2e4ff5b1ae
angelle rina <angellerinac@gmail.com>
2020-10-04 22:06:03 -0400

    tooltip d3 plugin + mouse events animation + delete click events

--- graph.js
+++ graph.js
@@ -38,6 +38,18 @@ const legend = d3
   .shapePadding(10)
   .scale(colour);
 
+const tip = d3
+  .tip()
+  .attr("class", "tip card")
+  .html((d) => {
+    let content = `<div class="name">${d.data.name}</div>`;
+    content += `<div class="cost">$${d.data.cost}</div>`;
+    content += `<div class="delete">Click slice to delete</div>`;
+    return content;
+  });
+
+graph.call(tip);
+
 // update function
 const update = (data) => {
   // update colour scale domain
@@ -70,6 +82,19 @@ const update = (data) => {
     .transition()
     .duration(750)
     .attrTween("d", arcTweenEnter);
+
+  // add events
+  graph
+    .selectAll("path")
+    .on("mouseover", (d, i, n) => {
+      tip.show(d, n[i]);
+      handleMouseOver(d, i, n);
+    })
+    .on("mouseout", (d, i, n) => {
+      tip.hide();
+      handleMouseOut(d, i, n);
+    })
+    .on("click", handleClick);
 };
 
 // data array and firestore
@@ -121,7 +146,6 @@ const arcTweenExit = (d) => {
 
 // use function keyword to allow use of 'this'
 function arcTweenUpdate(d) {
-  console.log(this._current, d);
   // interpolate between the two objects
   var i = d3.interpolate(this._current, d);
   // update the current prop with new updated data
@@ -132,3 +156,25 @@ function arcTweenUpdate(d) {
     return arcPath(i(t));
   };
 }
+
+// event handlers
+const handleMouseOver = (d, i, n) => {
+  //console.log(n[i]);
+  d3.select(n[i])
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", "#fff");
+};
+
+const handleMouseOut = (d, i, n) => {
+  //console.log(n[i]);
+  d3.select(n[i])
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", colour(d.data.name));
+};
+
+const handleClick = (d) => {
+  const id = d.data.id;
+  db.collection("expenses").doc(id).delete();
+};
