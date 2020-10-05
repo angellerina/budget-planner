b79b04a39f401fc65339a28e31c4033bd5543c58
angelle rina <angellerinac@gmail.com>
2020-10-04 21:33:04 -0400

    drawing the enter selection

--- graph.js
+++ graph.js
@@ -26,7 +26,16 @@ const arcPath = d3
 
 // update function
 const update = (data) => {
-  console.log(data);
+  // Join enhanced (pie) data to path elements
+  const paths = graph.selectAll("path").data(pie(data));
+
+  paths
+    .enter()
+    .append("path")
+    .attr("class", "arc")
+    .attr("d", arcPath)
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 3);
 };
 
 // data array and firestore
